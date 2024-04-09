40dc34fe74098955644bab092864039808a8da44
ZAlexanderV <alexandr_zft@mail.ru>
2024-04-09 21:24:03 +0500

    Output, locals added

---
+++ kubernetes/terraform/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  list-workers = [
+    for worker in(yandex_compute_instance.worker_node)[*] :
+    {
+      "name" : worker.name
+      "ip" : worker.network_interface.0.nat_ip_address
+      "vars" : { "ansible_user" = "clouduser", "ansible_ssh_private_key_file" = "~/otus/yc_key_priv" }
+    }
+  ]
+}
---
+++ kubernetes/terraform/output.tf
@@ -0,0 +1,10 @@
+output "controler_ip" {
+  value = {
+    name      = yandex_compute_instance.control_node1.name
+    public_ip = yandex_compute_instance.control_node1.network_interface.0.nat_ip_address
+  }
+}
+
+output "workers_ips" {
+  value = local.list-workers
+}
